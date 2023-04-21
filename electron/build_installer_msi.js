39280abe537bb73847723980bb31e9a5612ef6b4
kishore-maria <pmkish02@gmail.com>
2023-04-21 15:58:58 +0530

    fixing yml file generation

--- electron/build_installer_msi.js
+++ electron/build_installer_msi.js
@@ -7,7 +7,7 @@ const path = require('path');
 // 2. Define input and output directory.
 // Important: the directories must be absolute, not relative e.g
 // appDirectory: "C:\\Users\sdkca\Desktop\OurCodeWorld-win32-x64", 
-const APP_DIR = path.resolve('./angular-electron-practice-win32-x64');
+const APP_DIR = path.resolve('./output/angular-electron-practice-win32-x64');
 // const APP_DIR = path.resolve('D:\Projects\angular-electron-practice\electron\angular-electron-practice-win32-x64');
 // outputDirectory: "C:\\Users\sdkca\Desktop\windows_installer", 
 const OUT_DIR = path.resolve(__dirname, './windows_installer');
